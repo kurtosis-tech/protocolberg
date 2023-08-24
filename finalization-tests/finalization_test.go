b780c79961b3bd72f6d6fedbd7ff3087ba247da9
Gyanendra Mishra <anomaly.the@gmail.com>
2023-08-24 14:57:33 +0100

    work in progress

--- finalization-tests/finalization_test.go
+++ finalization-tests/finalization_test.go
@@ -5,12 +5,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/kurtosis-tech/kurtosis/api/golang/core/kurtosis_core_rpc_api_bindings"
+	"github.com/kurtosis-tech/kurtosis/api/golang/core/lib/services"
 	"github.com/kurtosis-tech/kurtosis/api/golang/engine/lib/kurtosis_context"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"sync"
 	"testing"
 	"time"
 )
@@ -26,6 +29,11 @@ const (
 	pathToMainFile = ""
 	// the main function is called run
 	runFunctionName = ""
+
+	beaconServiceHttpPortId   = "http"
+	finalizationRetryInterval = time.Second * 10
+	// 3 seconds per slot, 160 slots, some buffer
+	timeoutForFinalization = 3 * 180 * time.Second
 )
 
 var noExperimentalFeatureFlags = []kurtosis_core_rpc_api_bindings.KurtosisFeatureFlag{}
@@ -70,12 +78,47 @@ func TestEth2Package_DenebCapellaFinalization(t *testing.T) {
 	require.Nil(t, packageRunResult.InterpretationError)
 	require.Empty(t, packageRunResult.ValidationErrors)
 	require.Nil(t, packageRunResult.ExecutionError)
+
+	var beaconNodes []*services.ServiceContext
+	enclaveServices, err := enclaveCtx.GetServices()
+	require.Nil(t, err)
+	for serviceName := range enclaveServices {
+		serviceNameStr := string(serviceName)
+		if strings.HasPrefix(serviceNameStr, "cl-") && !strings.HasSuffix(serviceNameStr, "-validator") {
+			logrus.Info("Found beacon node with name '%v'", serviceNameStr)
+			beaconService, err := enclaveCtx.GetServiceContext(serviceNameStr)
+			require.NoError(t, err)
+			beaconNodes = append(beaconNodes, beaconService)
+		}
+	}
+
+	wg := sync.WaitGroup{}
+	for _, beaconNodeServiceCtx := range beaconNodes {
+		go func(beaconNodeServiceCtx *services.ServiceContext) {
+			for {
+				wg.Add(1)
+				privatePorts := beaconNodeServiceCtx.GetPrivatePorts()
+				httpPort, found := privatePorts[beaconServiceHttpPortId]
+				require.True(t, found)
+				epochsFinalized := getFinalization(t, httpPort.GetNumber())
+				logrus.Infof("Queried service '%s' got finalized epoch '%d'", beaconNodeServiceCtx.GetServiceName(), epochsFinalized)
+				if epochsFinalized > 0 {
+					wg.Done()
+				}
+				logrus.Infof("Pausing querying service '%s' for '%d' seconds", beaconNodeServiceCtx.GetServiceName(), finalizationRetryInterval)
+				time.Sleep(finalizationRetryInterval)
+			}
+		}(beaconNodeServiceCtx)
+	}
+
+	didWaitTimeout := waitTimeout(wg, timeoutForFinalization)
+	require.False(t, didWaitTimeout)
 }
 
 // extract this as a function that returns finalized epoch
-func TestGetFinalization(t *testing.T) {
+func getFinalization(t *testing.T, beaconHttpPort uint16) int {
 	finalizationEndpoint := "eth/v1/beacon/states/head/finality_checkpoints"
-	url := fmt.Sprintf("http://0.0.0.0:%d/%s", 59687, finalizationEndpoint)
+	url := fmt.Sprintf("http://0.0.0.0:%d/%s", beaconHttpPort, finalizationEndpoint)
 	resp, err := http.Get(url)
 	require.Empty(t, err, "an unexpected error happened while making http request")
 	require.NotNil(t, resp.Body)
@@ -86,4 +129,19 @@ func TestGetFinalization(t *testing.T) {
 	finalizedEpoch, err := strconv.Atoi(finalizedResponse.Data.Finalized.Epoch)
 	require.NoError(t, err, "an error occurred while converting finalized epoch to integer")
 	require.GreaterOrEqual(t, finalizedEpoch, 0)
+	return finalizedEpoch
+}
+
+func waitTimeout(wg *sync.WaitGroup, timeout time.Duration) bool {
+	c := make(chan struct{})
+	go func() {
+		defer close(c)
+		wg.Wait()
+	}()
+	select {
+	case <-c:
+		return false // completed normally
+	case <-time.After(timeout):
+		return true // timed out
+	}
 }
